7d3743a23e256b07890feeae7e7ceec37f8891af
Tatenda <bandambiratatenda@gmail.com>
2024-03-06 22:38:31 +0200

    First commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// get current year
+(function () {
+    var year = new Date().getFullYear();
+    document.querySelector("#currentYear").innerHTML = year;
+})();
+
+//attempt 1
+function doSomething(){
+    document.getElementById('')
+}
